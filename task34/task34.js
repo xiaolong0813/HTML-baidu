c29fddcb282c07a81fb990a997ac7bac9ff7fc90
Leo Xu <xuxiaolong200691@163.com>
2016-12-21 17:30:11 +0800

    task34完成

--- task34/task34.js
+++ task34/task34.js
@@ -8,18 +8,31 @@ var fill = function() {
     appendHtml(e('.square'), t)
 }
 //////////////////////////////////////
-var Diamond = function(dia) {
-    this.dia = dia,
-    this.deg = 0,
-    this.top = 0,
-    this.left = 0,
-    onAnimate = false
+var Diamond = function(box) {
+    this.box = box,
+    this.dia,
+    this.deg,
+    this.top,
+    this.left
+}
+Diamond.prototype.createAndSet = function() {
+    var t = `<div class="diamond"></div>`,
+        self = this
+    appendHtml(self.box, t)
+    self.dia = e('.diamond')
+    self.top = Math.floor(Math.random() * 9) * 40
+    self.left = Math.floor(Math.random() * 9) * 40
+    // self.deg = Math.floor(Math.random() * 3) * 90
+    self.deg = 0        //降低复杂度
+    self.dia.style.top = `${self.top}px`
+    self.dia.style.left = `${self.left}px`
+    self.dia.style.transform = `rotate(${self.deg}deg)`
 }
 Diamond.prototype.move = function() {
     var self = this
     return {
         forword : function() {
-            var dir = self.deg % 360
+            var dir = self.deg
             if ((dir === -90 || dir === 270) && (self.top > 0)) {
                 self.top -= 40
                 self.dia.style.top = `${self.top}px`
@@ -34,27 +47,28 @@ Diamond.prototype.move = function() {
                 self.dia.style.left = `${self.left}px`
             }
         },
-        turnLeft : function() {
-            log(self.deg)
-            self.deg -= 90
+        rotate : function(deg) {
+            // log(self.deg)
+            self.deg += deg
             self.dia.style.transition = '1s'
             self.dia.style.transform = `rotate(${self.deg}deg)`
             // 由于存在变量提升，必须等transition动画之后再执行下面的代码，将360变为0°
-            if (self.deg === -360) {
+            if (Math.abs(self.deg) >= 360) {
                 setTimeout(function(){
-                    self.deg = 0
+                    self.deg = self.deg % 360
                     self.dia.style.transition = '0s'
-                    self.dia.style.transform = `rotate(0deg)`
+                    self.dia.style.transform = `rotate(${self.deg}deg)`
                 },1000)
             }
         },
+        turnLeft : function() {
+            self.move().rotate(-90)
+        },
         turnRight : function() {
-            self.deg += 90
-            self.dia.style.transform = `rotate(${self.deg}deg)`
+            self.move().rotate(90)
         },
         turnBack : function() {
-            self.deg += 180
-            self.dia.style.transform = `rotate(${self.deg}deg)`
+            self.move().rotate(180)
         },
         transLeft : function() {
             if (self.left > 0) {
@@ -79,46 +93,91 @@ Diamond.prototype.move = function() {
                 self.top += 40
                 self.dia.style.top = `${self.top}px`
             }
+        },
+        moveLeft : function() {
+            self.dia.style.transform = `rotate(-180deg)`
+            self.move().transLeft()
+        },
+        moveTop : function() {
+            self.dia.style.transform = `rotate(-90deg)`
+            self.move().transTop()
+        },
+        moveRight : function() {
+            self.dia.style.transform = `rotate(0deg)`
+            self.move().transRight()
+        },
+        moveBottom : function() {
+            self.dia.style.transform = `rotate(90deg)`
+            self.move().transBottom()
         }
     }
 }
 Diamond.prototype.getMsg = function(msg) {
     var self = this
-    // return {
-    //     response : function() {
     var msg = msg.toLowerCase()
-    if (msg === 'go') {
-        self.move().forword()
-    } else if (msg === 'tun lef') {
-        self.move().turnLeft()
-    } else if (msg === 'tun rig') {
-        self.move().turnRight()
-    } else if (msg === 'tun bac') {
-        self.move().turnBack()
+    switch (msg) {
+        case 'go':
+            self.move().forword()
+            break;
+        case 'tun lef':
+            self.move().turnLeft()
+            break;
+        case 'tun rig':
+            self.move().turnRight()
+            break;
+        case 'tun bac':
+            self.move().turnBack()
+            break;
+        case 'tra lef':
+            self.move().transLeft()
+            break;
+        case 'tra top':
+            self.move().transTop()
+            break;
+        case 'tra rig':
+            self.move().transRight()
+            break;
+        case 'tra bot':
+            self.move().transBottom()
+            break;
+        case 'mov lef':
+            self.move().moveLeft()
+            break;
+        case 'mov top':
+            self.move().moveTop()
+            break;
+        case 'mov rig':
+            self.move().moveRight()
+            break;
+        case 'mov bot':
+            self.move().moveBottom()
+            break;
+        default:
+            alert('无效指令！')
     }
-        // }
-    // }
 }
 ////////////绑定输入命令事件
 var bindButton = function() {
     var btn = e('#id-button')
     var input = e('#id-input')
+    var count = 0
     bindEvent(btn, 'click', function(e){
         var value = input.value
-        // log(this)
         this.getMsg(value)
+        //为防止重复调用动画，在点击后将按钮失效，等1s后(动画结束后),再使其有效
+        btn.disabled = true
+        setTimeout(function(){
+            btn.disabled = false
+        },1000)
     }.bind(this))
-}
-var bindTransitionEnd = function() {
-    bindEvent(e('.diamond'), 'transitionend', function(e){
-        log('end')
-    })
 }
 
 var __main = function() {
     fill()
-    var dia = e('.diamond')
-    var d = new Diamond(dia)
+    // var dia = e('.diamond')
+    var sq = e('.square')
+    var d = new Diamond(sq)
+    d.createAndSet()
     bindButton.call(d)
-    bindTransitionEnd()
+    // bindTransitionEnd()
 }
